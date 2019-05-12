29d36f18bfdc983158e978767c7151f5da1d5ec1
roiroy <roi-tools@protonmail.com>
2019-05-12 19:02:01 +0300

    fractions/decimals option

--- src/App.js
+++ src/App.js
@@ -42,21 +42,43 @@ class CalculatorRoot extends React.Component {
     this.onBuildTreeToggle = this.onBuildTreeToggle.bind(this)
     this.onTotalsTreeToggle = this.onTotalsTreeToggle.bind(this)
     this.onInputChange = this.onInputChange.bind(this)
-    this.state = this._recompute({}, 'BerryPie', Fraction(8, 15))
+    this.onSettingsChange = this.onSettingsChange.bind(this)
+    this.pf = this.pf.bind(this)
+    this.state = {
+      settings: {
+        numbers: 'fractions',
+      }
+    }
+    this.state = this._recompute(this.state, 'ChickenMeat', Fraction(11, 15))
   }
 
   _recompute(target, product, qty_per_day) {
     const buildTree = this.props.calculator.buildTree(product, qty_per_day)
     const totals = this.props.calculator.totals(buildTree)
     const grandTotal = Object.values(totals).map(t => t.totalFactoryCost).reduce((a, b) => a + b, 0)
-    return Object.assign(target, {
+    const calculationState = {
       recipe: product,
       requiredQty: qty_per_day,
       tree: buildTree,
-      treebeardTree: this.toTreebeardTree(buildTree),
       totals: totals,
-      treebeardTotals: this.toTreebeardTotals(totals, grandTotal),
-    })
+      grandTotal: grandTotal,
+    }
+    return Object.assign(target, calculationState, this._trees(calculationState))
+  }
+
+  _trees(state) {
+    return {
+      treebeardTree: this.toTreebeardTree(state.tree),
+      treebeardTotals: this.toTreebeardTotals(state.totals, state.grandTotal),
+    }
+  }
+
+  onSettingsChange(newSettings) {
+    this.setState((prevState) => ({
+      settings: Object.assign(prevState.settings, newSettings)
+    }))
+    // double update but meh
+    this.setState((prevState) => this._trees(prevState))
   }
 
   render() {
@@ -67,6 +89,7 @@ class CalculatorRoot extends React.Component {
         <h4>What and how much do you desire?</h4>
         <CalculatorInput recipes={recipes}
           recipe={this.state.recipe} requiredQty={this.state.requiredQty} onChange={this.onInputChange} />
+        <CalculatorSettings settings={this.state.settings} onChange={this.onSettingsChange} />
         <h4>Build tree</h4>
         {this.buildTreeHeader()}
         <Treebeard data={this.state.treebeardTree} onToggle={this.onBuildTreeToggle} style={TreebeardTheme} />
@@ -134,7 +157,7 @@ class CalculatorRoot extends React.Component {
         name: (<Fragment>{this._recipeHeader(output, totals.demand_per_day.mul(15), totals.total, totals.factory)}{this._renderCosts(totals)}</Fragment>),
         toggled: false,
         children: totals.towards.length <= 1 ? [] : totals.towards.map(t => Object.assign({
-          name: (<Fragment>{t.recipe} {pf(t.recipeQty)} ({pf(t.fraction)} of all)</Fragment>),
+          name: (<Fragment>{t.recipe} {this.pf(t.recipeQty)} ({this.pf(t.fraction)} of all)</Fragment>),
           toggled: false,
           children: null,
         }))
@@ -153,8 +176,8 @@ class CalculatorRoot extends React.Component {
   _recipeHeader(output, demand, recipeQty, factory, extraClassName='') {
     return (<Fragment>
       <span className={"productName " + extraClassName}>{output}</span>
-      <span className="demand">{pf(demand)}</span>
-      <span className="recipeQty">{pf(recipeQty)}</span>
+      <span className="demand">{this.pf(demand)}</span>
+      <span className="recipeQty">{this.pf(recipeQty)}</span>
       <span className="factory">{factory}</span>
       </Fragment>)
   }
@@ -191,6 +214,10 @@ class CalculatorRoot extends React.Component {
 
   onTotalsTreeToggle(node, toggled) {
     this._onTreeToggle(node, toggled, 'treebeardTotals', 'totalsCursor')
+  }
+
+  pf(f) {
+    return pf(f, this.state.settings.numbers)
   }
 }
 
@@ -242,14 +269,42 @@ class CalculatorInput extends React.Component {
   }
 }
 
-function pf(f) {
+
+class CalculatorSettings extends React.Component {
+  constructor(props) {
+    super(props)
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(e) {
+    const { name, value } = e.target;
+    this.props.onChange({[name]: value})
+  }
+
+  render() {
+    return (
+      <div id='settings'>
+        <input id='fractions' type='radio' name='numbers' value='fractions' checked={this.props.settings.numbers === 'fractions'} onChange={this.onChange}/>
+        <label for='fractions'>fractions</label>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <input id='decimals' type='radio' name='numbers' value='decimals' checked={this.props.settings.numbers === 'decimals'} onChange={this.onChange}/>
+        <label for='decimals'>decimals</label>
+      </div>
+    )
+  }
+}
+
+
+function pf(f, format='fractions') {
   if (f === null || f === undefined) return '<NOOO>'
   if (f.n === 0) return '0'
+  if (format !== 'fractions') {
+    return f.valueOf().toFixed(2)
+  }
   const _render_proper_fraction = (n, d) => (<Fragment><sup>{n}</sup>&frasl;<sub>{d}</sub></Fragment>)
-  // return f.n.toString() + "/" + f.d.toString()
   let i = Math.floor(f.n / f.d)
   let r = f.n % f.d
-  return (<Fragment>{i === 0 ? '' : i}{r === 0 ? '' : _render_proper_fraction(f.n - f.d * i, f.d)}</Fragment>)
+  return (<Fragment>{i === 0 ? '' : i}{r === 0 ? '' : _render_proper_fraction(r, f.d)}</Fragment>)
 }
 
 function pc(c) {
